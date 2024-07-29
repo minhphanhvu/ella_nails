3735d4326429941212971a8c0c82464144cb24b2
minhphanhvu <minhphanh.vu@gmail.com>
2024-07-28 22:55:10 -0500

    remove dependent destroy for nail service template model

--- app/models/nail_service_template.rb
+++ app/models/nail_service_template.rb
@@ -1,6 +1,6 @@
 class NailServiceTemplate < ApplicationRecord
   belongs_to :service_category
-  has_many :nail_services, dependent: :destroy
+  has_many :nail_services
 
   validates_presence_of :name, :duration, :price
 end
